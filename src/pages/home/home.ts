5959e35615c430a73ef9ceccf4a72bb24a8af72d
Jociane Duarte <jocyane24@gmail.com>
2018-11-28 18:20:47 -0500

    câmera e vibração após salvar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Vibration } from '@ionic-native/vibration';
 
 @Component({
   selector: 'page-home',
@@ -7,12 +9,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  foto: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public camera: Camera, private vibration: Vibration) {
   }
 
-  selectChange(e){
-    console.log(e);
+  defOptions(){
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    return options;
   }
+
+  tirarFoto(){
+    this.camera.getPicture(this.defOptions()).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      this.foto = 'data:image/jpeg;base64,' + imageData;
+     }, (err) => {
+      // Handle error
+     });
+  }
+
+  salvarMulher(){
+    this.vibration.vibrate(1000);
+  }
+
 
 }
