688facb27dc981ed495a4de5f0c3826bff4b9749
Jociane Duarte <jocyane24@gmail.com>
2018-11-28 18:40:07 -0500

    Correção no app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { Camera } from '@ionic-native/camera';
+import { Vibration } from '@ionic-native/vibration';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -29,6 +30,8 @@ import { IonicStepperModule } from 'ionic-stepper';
   providers: [
     StatusBar,
     SplashScreen,
+    Camera,
+    Vibration,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
